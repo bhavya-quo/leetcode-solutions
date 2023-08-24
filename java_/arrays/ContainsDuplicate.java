f73ff5040c517ef4fccdd5c4f5f2c51ad0ddb45c
Bhavya Kamboj <bhavya.kamboj@walmart.com>
2023-08-24 10:56:19 +0530

    solution for 217 - contains duplicate

--- java_/arrays/ContainsDuplicate.java
+++ java_/arrays/ContainsDuplicate.java
@@ -1,4 +1,24 @@
 package java_.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && map.get(nums[i]) >= 1) {
+                return true;
+            }
+            map.put(nums[i], 1);
+            // map.put(nums[i], map.getOrDefault(nums[i], 0) + 1); // this approach is less efficient and slow
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicate program = new ContainsDuplicate();
+        System.out.println(program.containsDuplicate(new int[]{1, 1, 2, 3, 4, 5}));
+        System.out.println(program.containsDuplicate(new int[]{1,2,3}));
+    }
 }
