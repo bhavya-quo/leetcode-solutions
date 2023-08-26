cc3bc1e0e5f282db49790c51c317c327b0a0690a
bhavyakamboj <bhavya.kamboj@gmail.com>
2023-08-26 06:40:32 +0530

    added solutions for ContainsDuplicate and RemoveDuplicatesFromSortedArray

--- java_/arrays/ContainsDuplicate.java
+++ java_/arrays/ContainsDuplicate.java
@@ -1,4 +1,30 @@
 package java_.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && map.get(nums[i]) >= 1) {
+                return true;
+            }
+            map.put(nums[i], 1);
+            // map.put(nums[i], map.getOrDefault(nums[i], 0) + 1); // this approach is less efficient and slow
+        }
+        return false;
+    }
+}
+// generate test cases for above code
+class ContainsDuplicateTest {
+    public static void main(String[] args) {
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        int[] nums1 = {1, 2, 3, 1};
+        System.out.println(containsDuplicate.containsDuplicate(nums1));
+        int[] nums2 = {1, 2, 3, 4};
+        System.out.println(containsDuplicate.containsDuplicate(nums2));
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(containsDuplicate.containsDuplicate(nums3));
+    }
 }
--- java_/arrays/RemoveDuplicatesFromSortedArray.java
+++ java_/arrays/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,25 @@
 package java_.arrays;
 
 public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int insertIndex = 1;
+        for (int i = 1; i < n; i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+}
+
+// create test cases for removeDuplicates method
+class RemoveDuplicatesFromSortedArrayTest {
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int result = removeDuplicatesFromSortedArray.removeDuplicates(nums);
+        System.out.println(result);
+    }
 }
